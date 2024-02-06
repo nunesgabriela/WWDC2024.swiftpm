4fc0a49af7edc124e42e352d5f5afe85da8f7a62
Gabriela Nunes <nunesxg@gmail.com>
2024-02-06 09:47:27 -0300

    Configuring fonts

---
+++ Fonts/Fonts+.swift
@@ -0,0 +1,16 @@
+//
+//  Fonts+.swift
+//  WWDC2024
+//
+//  Created by Gabriela Nunes on 06/02/24.
+//
+
+import SwiftUI
+
+extension Font {
+    public static var title: Font {
+        get {
+            .custom("Poppins-Light", size: 36)
+        }
+    }
+}
